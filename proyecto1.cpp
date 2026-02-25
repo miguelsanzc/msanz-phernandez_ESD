5529a46b0038c8ca7fe889420e3ec9448ecfac03
miguelsanzc <miguel.sanzc@alumnos.upm.es>
2026-02-25 20:02:56 +0100

    subida inicial de archivo de prueba readme y de proyecto

---
+++ proyecto1.cpp
@@ -0,0 +1,13 @@
+//includes
+//Structs
+struct i2c_rdwr_ioctl_data packets;
+struct i2c_msg messages[6];
+char write_bytes[w_len];
+char read_bytes[r_len];
+
+int main(){
+	
+	char i2cFile[15];
+	int addr = 0x5d;
+	sprintf(i2cFile,"",);
+}
